0f2ba37a133eb94ae2dd72ff08d3c27e1fe1d968
Regis Kuckaertz <regis_kuckaertz@34452.gnm.int>
2018-04-28 15:46:08 +0100

    Add cta Add explainer

--- src/Data/Capi/Types/Cta.hs
+++ src/Data/Capi/Types/Cta.hs
@@ -1,3 +1,17 @@
 module Data.Capi.Types.Cta where
 
+import GHC.Generics
 import Data.Aeson
+import Data.Text
+
+data CTAAtom =
+  CTAAtom { _url :: String
+          , _backgroundImage :: Text
+          , _btnText :: Text
+          , _label :: Maybe Text
+          , _trackingCode :: Maybe Text
+          }
+  deriving (Show, Eq, Ord, Generic)
+
+instance ToJSON CTAAtom
+instance FromJSON CTAAtom
--- src/Data/Capi/Types/Explainer.hs
+++ src/Data/Capi/Types/Explainer.hs
@@ -1,3 +1,44 @@
-module Data.Capi.Types.Explainer where
+module Data.Capi.Types.Explainer
+  ( DisplayType
+  , flat
+  , expandable
+  , carousel
+  , ExplainerAtom(..)
+  ) where
 
+import GHC.Generics
 import Data.Aeson
+import Data.Text
+
+newtype DisplayType = DT Int deriving (Show, Eq, Ord)
+
+instance FromJSON DisplayType where
+  parseJSON = withText "DisplayType" $ \t -> case unpack t of
+    "flat" -> pure flat
+    "expandable" -> pure expandable
+    _ -> pure carousel
+
+instance ToJSON DisplayType where
+  toJSON dt | dt == flat = String (pack "flat")
+            | dt == expandable = String (pack "expandable")
+            | otherwise = String (pack "carousel")
+
+flat :: DisplayType
+flat = DT 0
+
+expandable :: DisplayType
+expandable = DT 1
+
+carousel :: DisplayType
+carousel = DT 2
+
+data ExplainerAtom =
+  ExplainerAtom { _title :: Text
+                , _body :: Text
+                , _displayType :: DisplayType
+                , _tags :: Maybe [Text]
+                }
+  deriving (Show, Eq, Ord, Generic)
+
+instance FromJSON ExplainerAtom
+instance ToJSON ExplainerAtom
