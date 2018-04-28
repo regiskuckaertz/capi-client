38fba68d8eb00439a01f6724cc3280025b9bac0b
Regis Kuckaertz <regis_kuckaertz@34452.gnm.int>
2018-04-28 15:17:28 +0100

    Bootstrap CommonsDivision

--- src/Data/Capi/Types/Commonsdivision.hs
+++ src/Data/Capi/Types/Commonsdivision.hs
@@ -1,3 +1,32 @@
 module Data.Capi.Types.Commonsdivision where
 
+import GHC.Generics
 import Data.Aeson
+import Data.Capi.Types.Shared(DateTime)
+import Data.Text
+
+data MP = MP { _name :: String, _party :: String }
+  deriving (Show, Eq, Ord, Generic)
+
+instance FromJSON MP
+instance ToJSON MP
+
+data Votes = Votes { _ayes :: [MP], _noes :: [MP] }
+  deriving (Show, Eq, Ord, Generic)
+
+instance FromJSON Votes
+instance ToJSON Votes
+
+data CommonsDivision = 
+  CommonsDivision { _parliamentId :: Text
+                  , _description :: Maybe Text
+                  , _date :: DateTime
+                  , _votes :: Votes
+                  }
+  deriving (Show, Eq, Ord)
+
+instance ToJSON CommonsDivision where
+  toJSON cd = _
+
+instance FromJSON CommonsDivision where
+  parseJSON = _
