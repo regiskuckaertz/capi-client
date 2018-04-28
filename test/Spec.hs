e1625644ab12c45dd92729cae8d8a4cccc707078
Regis Kuckaertz <regis_kuckaertz@34452.gnm.int>
2018-04-28 18:23:07 +0100

    Bootstrap QuickCheck

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,14 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Data.Aeson
+import Data.Capi.Types
+import Data.Text
+import Test.QuickCheck
+
+instance Arbitrary User where
+  arbitrary = User <$> (pack <$> arbitrary) 
+                    <*> (Just . pack <$> arbitrary) 
+                    <*> (Just . pack <$> arbitrary)
+
+prop_ContentChangeDetails u =
+  decode (encode u) == Just u where types = u :: User
+
+main = putStrLn "Hi"
